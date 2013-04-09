b7b1f85282d8203ccdf70049dade1a7ebf0f389e
Emma Lindsay <emma@flickme.com>
2013-04-08 20:02:55 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Piggylet::Application.config.secret_token = '1dea397dcd7f8344fba38c2fa7d6ba09469cc1233b960cc16daa9730fb8c37323e497b1fdd4fbc0f2d2920364b0f70f52bf1cee2a1349745e16f140f9a7a0d53'
